25843492abd180dac6b388bee51a94b242d2b6f9
Ankit Goyal <ankit.goyal@sixt.com>
2021-09-18 15:58:22 +0530

    First draft of GetValue API, also fixed a bug in SetKeyValue API

--- handler/keyValue.go
+++ handler/keyValue.go
@@ -13,15 +13,19 @@ type KeyValueHandler struct{
 	InMemoryRepository repository.InMemoryClient
 }
 
-func (h *KeyValueHandler) setKeyValue(w http.ResponseWriter, r *http.Request) {
+func (h *KeyValueHandler) SetKeyValue(w http.ResponseWriter, r *http.Request) {
 	//add logs
-	//input validation ? - not really required 
+	//input validation ? - not really required
 	//parse req body
 	//call repo
 	//return response
+	//http error codes
+
 	ctx := context.Background()
 	setKeyValueRequest := contract.SetKeyValueRequest{}
 	json.NewDecoder(r.Body).Decode(&setKeyValueRequest)
+
+	//handler error
 	h.InMemoryRepository.SetKeyValue(ctx,setKeyValueRequest.Key,setKeyValueRequest.Value)
 
 	setKeyValueResponse := contract.SetKeyValueResponse{
@@ -31,5 +35,35 @@ func (h *KeyValueHandler) setKeyValue(w http.ResponseWriter, r *http.Request) {
 	//handle err
 	resp, _ := json.Marshal(setKeyValueResponse)
 
+	//how to say content type
+	w.Write(resp)
+}
+
+func (h *KeyValueHandler) GetValue(w http.ResponseWriter, r *http.Request) {
+	//add logs
+	//input validation ? - not really required
+	//parse req body
+	//call repo
+	//return response
+	values :=  r.URL.Query()
+	keyValues, found := values["key"]
+	if !found {
+		//throw err
+	}
+	if len(keyValues) > 1 {
+		//too many keys //Bad Request
+	}
+
+	ctx := context.Background()
+	value := h.InMemoryRepository.GetValue(ctx,keyValues[0])
+
+	getValueResponse := contract.GetValueResponse{
+		Key: keyValues[0],
+		Value: value,
+	}
+	//handle err
+	resp, _ := json.Marshal(getValueResponse)
+
+	//how to say content type
 	w.Write(resp)
 }
--- handler/routes.go
+++ handler/routes.go
@@ -4,5 +4,8 @@ import "net/http"
 
 func HandleRequests(keyValueHandler KeyValueHandler) {
 	//How do you say its a POST?
-	http.HandleFunc("/setKeyValue", keyValueHandler.setKeyValue)
+	http.HandleFunc("/setKeyValue", keyValueHandler.SetKeyValue)
+
+	//How do you say its a GET with a query param?
+	http.HandleFunc("/getValue", keyValueHandler.GetValue)
 }
