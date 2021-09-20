f0640ff5c0a7543ad0cc1adf7c9afe735e7a0dc3
Ankit Goyal <ankit.goyal@sixt.com>
2021-09-20 23:54:07 +0530

    Added tests n refactored a bit

--- handler/keyValue.go
+++ handler/keyValue.go
@@ -9,6 +9,11 @@ import (
 	"net/http"
 )
 
+//error msgs
+const (
+	missingQueryParamMsg = "Missing Query Param"
+	notFoundMsg = "Not Found"
+)
 
 type KeyValueHandler struct{
 	InMemoryRepository repository.InMemoryClient
@@ -22,7 +27,7 @@ func (h *KeyValueHandler) SetKeyValue(ctx context.Context,w http.ResponseWriter,
 	setKeyValueRequest := contract.SetKeyValueRequest{}
 	err := json.NewDecoder(r.Body).Decode(&setKeyValueRequest)
 	if err!=nil {
-		http.Error(w,"Bad Request",http.StatusBadRequest)
+		http.Error(w,badRequestMsg,http.StatusBadRequest)
 		return
 	}
 	log.Printf("Key : %s, Value %s",setKeyValueRequest.Key,setKeyValueRequest.Value)
@@ -31,7 +36,7 @@ func (h *KeyValueHandler) SetKeyValue(ctx context.Context,w http.ResponseWriter,
 	err = h.InMemoryRepository.SetKeyValue(ctx,setKeyValueRequest.Key,setKeyValueRequest.Value)
 	if err!= nil {
 		log.Println("Repository Error calling SetKeyValue")
-		http.Error(w,"Something went wrong!",http.StatusInternalServerError)
+		http.Error(w,internalServerErrorMsg,http.StatusInternalServerError)
 		return
 	}
 
@@ -43,7 +48,7 @@ func (h *KeyValueHandler) SetKeyValue(ctx context.Context,w http.ResponseWriter,
 	resp, err := json.Marshal(setKeyValueResponse)
 	if err!= nil {
 		log.Println("Error in JSON Marshaling")
-		http.Error(w,"Something went wrong!",http.StatusInternalServerError)
+		http.Error(w,internalServerErrorMsg,http.StatusInternalServerError)
 		return
 	}
 
@@ -59,19 +64,14 @@ func (h *KeyValueHandler) GetValue(ctx context.Context, w http.ResponseWriter, r
 	keyValues, found := values["key"]
 	if !found {
 		log.Println("Request could not be completed as key was missing in query params")
-		http.Error(w,"Missing Query Param",http.StatusBadRequest)
-		return
-	}
-	if len(keyValues) > 1 {
-		log.Println("Request could not be completed as there was no single key")
-		http.Error(w,"Too many parameters",http.StatusBadRequest)
+		http.Error(w,missingQueryParamMsg,http.StatusBadRequest)
 		return
 	}
 
 	value := h.InMemoryRepository.GetValue(ctx,keyValues[0])
 	if len(value) == 0 {
 		log.Println("Key not found")
-		http.Error(w,"Not Found",http.StatusNotFound)
+		http.Error(w,notFoundMsg,http.StatusNotFound)
 		return
 	}
 
@@ -83,7 +83,7 @@ func (h *KeyValueHandler) GetValue(ctx context.Context, w http.ResponseWriter, r
 	resp, err := json.Marshal(getValueResponse)
 	if err!= nil {
 		log.Println("Error in JSON Marshaling")
-		http.Error(w,"Something went wrong!",http.StatusInternalServerError)
+		http.Error(w,internalServerErrorMsg,http.StatusInternalServerError)
 		return
 	}
 
--- handler/keyValue_test.go
+++ handler/keyValue_test.go
@@ -1,1 +1,142 @@
 package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"github.com/discoverAnkit/getir/contract"
+	"github.com/discoverAnkit/getir/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type KeyValueHandlerTestSuite struct {
+	suite.Suite
+	InMemoryRepository  *mocks.InMemoryClient
+	keyValueHandler     *KeyValueHandler
+	router              *Router
+}
+
+func (s *KeyValueHandlerTestSuite) SetupTest() {
+	//No need to inject repositories in these objects as these tests are meant to only test routing
+	//and thus in these tests we wont reach those points where repo will get called
+	s.InMemoryRepository = new(mocks.InMemoryClient)
+	s.keyValueHandler = &KeyValueHandler{
+		InMemoryRepository: s.InMemoryRepository,
+	}
+	s.router = NewRouter(s.keyValueHandler,nil)
+}
+
+func Test_KeyValueHandlerTestSuite(t *testing.T) {
+	tests := new(KeyValueHandlerTestSuite)
+	suite.Run(t, tests)
+}
+
+func (s *KeyValueHandlerTestSuite) Test_SetKeyValue_BadRequest() {
+
+	req, _ := http.NewRequest(http.MethodPost, "https://api-stage.something.com/setKeyValue",strings.NewReader(`{\"key\":\"sonammm\",\"value\":1}`))
+	res := httptest.NewRecorder()
+
+	s.router.Handle(res,req)
+
+	s.Equal(http.StatusBadRequest, res.Code)
+	s.Equal(res.Body.String(),badRequestMsg+"\n")
+}
+
+func (s *KeyValueHandlerTestSuite) Test_SetKeyValue_InternalError() {
+
+	//Given
+	reqPayload := contract.SetKeyValueRequest{
+		Key: "abc",
+		Value: "def",
+	}
+	reqBody, _ := json.Marshal(reqPayload)
+	req, _ := http.NewRequest(http.MethodPost, "https://api-stage.something.com/setKeyValue", bytes.NewBuffer(reqBody))
+	res := httptest.NewRecorder()
+
+	s.InMemoryRepository.On("SetKeyValue",mock.Anything,reqPayload.Key,reqPayload.Value).Return(errors.New("something"))
+
+	//when
+	s.router.Handle(res,req)
+
+	//then
+	s.Equal(http.StatusInternalServerError, res.Code)
+	s.Equal(res.Body.String(),internalServerErrorMsg+"\n")
+
+}
+
+func (s *KeyValueHandlerTestSuite) Test_SetKeyValue_Successful() {
+
+	//Given
+	reqPayload := contract.SetKeyValueRequest{
+		Key: "abc",
+		Value: "def",
+	}
+	reqBody, _ := json.Marshal(reqPayload)
+	req, _ := http.NewRequest(http.MethodPost, "https://api-stage.something.com/setKeyValue", bytes.NewBuffer(reqBody))
+	res := httptest.NewRecorder()
+
+	s.InMemoryRepository.On("SetKeyValue",mock.Anything,reqPayload.Key,reqPayload.Value).Return(nil)
+
+	//when
+	s.router.Handle(res,req)
+
+	//then
+	var apiResp contract.GetValueResponse
+	json.Unmarshal(res.Body.Bytes(),&apiResp)
+	s.Equal(http.StatusOK, res.Code)
+	s.Equal(apiResp.Key,reqPayload.Key)
+	s.Equal(apiResp.Value,reqPayload.Value)
+}
+
+func (s *KeyValueHandlerTestSuite) Test_GetValue_MissingQueryParam() {
+
+	req, _ := http.NewRequest(http.MethodGet, "https://api-stage.something.com/getValue",nil)
+	res := httptest.NewRecorder()
+
+	s.router.Handle(res,req)
+
+	s.Equal(http.StatusBadRequest, res.Code)
+	s.Equal(res.Body.String(),missingQueryParamMsg+"\n")
+}
+
+func (s *KeyValueHandlerTestSuite) Test_GetValue_KeyNotFound() {
+
+	//Given
+	keyInput := "abc"
+	req, _ := http.NewRequest(http.MethodGet, "https://api-stage.something.com/getValue?key="+keyInput, nil)
+	res := httptest.NewRecorder()
+
+	s.InMemoryRepository.On("GetValue",mock.Anything,keyInput).Return("")
+
+	//when
+	s.router.Handle(res,req)
+
+	//then
+	s.Equal(http.StatusNotFound, res.Code)
+}
+
+func (s *KeyValueHandlerTestSuite) Test_GetValue_Successful() {
+
+	//Given
+	keyInput := "abc"
+	valueOutput := "def"
+	req, _ := http.NewRequest(http.MethodGet, "https://api-stage.something.com/getValue?key="+keyInput, nil)
+	res := httptest.NewRecorder()
+
+	s.InMemoryRepository.On("GetValue",mock.Anything,keyInput).Return(valueOutput)
+
+	//when
+	s.router.Handle(res,req)
+
+	//then
+	var apiResp contract.GetValueResponse
+	json.Unmarshal(res.Body.Bytes(),&apiResp)
+	s.Equal(http.StatusOK, res.Code)
+	s.Equal(apiResp.Key,keyInput)
+	s.Equal(apiResp.Value,valueOutput)
+}
